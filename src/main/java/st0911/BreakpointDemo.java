78ac413bd9809b81e34e1583034c2252e91afdd2
JIN <d1397026@o365.fcu.edu.tw>
2025-09-11 12:55:55 +0800

    0

---
+++ src/main/java/st0911/BreakpointDemo.java
@@ -0,0 +1,35 @@
+package st0911;
+
+import java.util.Scanner;
+
+public class BreakpointDemo {
+
+    public static void main(String[] args) {
+        System.out.println("要");
+        System.out.println("跳");
+        System.out.println("過");
+        System.out.println("嗎");
+
+        computeArea();
+
+        System.out.println("安");
+        System.out.println("安");
+        System.out.println("你");
+        System.out.println("好");
+        // resume 是直接跳到下一個中斷點
+        // step into 一行一行執行，最單純
+        // step over 跳過方法（但方法會執行，只是直接執行，不會進去方法內一步一步走）
+    }
+
+    public static void computeArea() {
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter diameter: ");
+//        int diameter = input.nextInt();
+
+        int diameter = 10;
+
+        double area = Math.PI * Math.pow(diameter / 2, 2);
+        System.out.println("diameter: " + diameter);
+        System.out.println("area: " + area);
+    }
+}
